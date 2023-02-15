fb491a14581650d087ffd91316cf8a73a0c62206
Kiran Rathod <kiranrathod606@gmail.com>
2023-02-15 10:52:00 +0530

    4th commit

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 <body>
 
 <?php
-$string= "HAPPY VELENTINE'S DAY DEAR";
-echo $string."<br>";
- echo "<br>";
-echo '<span style="color: red; font-size: 50px;">'; 
+$color = "red";
+echo "HAPPY VELENTINE'S DAY DEAR" . $color . "<br>"; 
 ?>
 
 </body>
